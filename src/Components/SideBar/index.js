e64e7f03160e83814b2f9627c469e7ca787771c0
Emre Memiş <113227617+memisemre@users.noreply.github.com>
2023-03-14 23:17:11 +0300

    OverAll System Ready

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
+import Logo from "../Logo/index";
+import { Categories } from "./utils";
+import { AiFillCaretDown } from "react-icons/ai";
+const SideBar = () => {
+  return (
+    <div className="sideBar">
+      <Logo />
+      <nav>
+        {Categories.map((navItem) => {
+          const { icon, categoryName, subCategories } = navItem;
+          return (
+            <div
+              style={{ color: "#9C2780" }}
+              className="navItem"
+              key={categoryName}
+            >
+              {icon} {categoryName} {subCategories && <AiFillCaretDown />}
+              <div>
+                {subCategories && (
+                  <div>
+                    {subCategories.map((subNavItem) => {
+                      const { categoryName, icon } = subNavItem;
+                      return (
+                        <div key={categoryName}>
+                          {categoryName} {icon}
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </nav>
+    </div>
+  );
+};
 
-function SideBar(){
-    return(
-        <div className='sidebar-container'>
-            sidebar
-        </div>
-    )
-}
 export default SideBar;
